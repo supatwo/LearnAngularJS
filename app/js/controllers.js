648f5fb2dacddadd44ca1decfa52b58575e26e8e
unknown <u0115967@U0115967-TPL-A.ten.thomsonreuters.com>
2013-10-08 10:27:21 +0700

    add model

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,6 +2,35 @@
 
 /* Controllers */
 
+angular.module('portfolio',[]).controller('PortfolioCtrl', function($scope){
 
+	$scope.model ={
+		stockName:'',
+		amount:'',
+		positions:[{name:'PTT', amount:1000, price:319,total:319000}]
+	};
+
+	$scope.addToPortfolio = function(stockName, amount, positions){
+		var stockQuote = new StockQuote();
+		var price = stockQuote.getPrice(stockName); 
+		//add code here
+	};
+
+	$scope.totalValue = function(positions) {
+		var total = 0;
+		//add code here
+		return total;
+	}
+});
+
+
+
+function StockQuote(){
+	this.maxPrice = 300;
+	this.minPrice = 20;
+	this.getPrice = function(stock){
+		return Math.random() * (this.maxPrice - this.minPrice);
+	}
+}
 
 
