c9ddd8f6a5f215fcdc9b618703f7a7b0faa99719
Supat Wongwirawat <supat.wongwirawat@thomsonreuters.com>
2013-10-08 12:19:08 +0700

    add provider

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -57,11 +57,48 @@ var PortfolioFactory = function (stockQuote) {
 		}
 }
 
+
 angular.module('portfolio',[])
 	.value('stockQuote', new StockQuote())	
 	.service('portfolioService', PortfolioService)
 	.factory('portfolioFactory', PortfolioFactory)
-	.controller('PortfolioCtrl', function($scope, portfolioFactory){
+	.provider('portfolioData', function(stockQuote){
+		    var config ={feed:""};
+			var positions = [{name:'PTTE', amount:1000, price:168, total:168000}];
+		return {
+			
+	
+			setFeed : function(feed){
+				config.feed = feed;
+			}
+
+			$get: function()
+			return {
+				addToPortfolio : function(stockName, amount, positions){
+					var price = stockQuote.getPrice(stockName); 
+					positions.push({
+						name:stockName, 
+						amount:amount,
+						price: price,
+						total: amount*price});
+				},
+				totalValue 		: function(positions) {
+					var total = 0;
+					for(var k=0; k<positions.length; k++){
+						total = total + positions[k].total;
+					}
+					return total;
+				},
+				positions : positions
+
+			}
+		}
+	})
+	.config(function(portfolioDataProvider){
+
+
+	}).
+	.controller('PortfolioCtrl', function($scope, portfolioData){
 		$scope.model ={
 			stockName:'',
 			amount:'',
@@ -73,4 +110,5 @@ angular.module('portfolio',[])
 
 		$scope.addToPortfolio = portfolioFactory.addToPortfolio;
 		$scope.totalValue = portfolioFactory.totalValue;
-	});
+	})
+
