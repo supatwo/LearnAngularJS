45642250a021e8fae1366856e0c185fb03390911
Supat Wongwirawat <supat.wongwirawat@thomsonreuters.com>
2013-10-08 11:17:23 +0700

    refactor to use portfolio service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,36 +2,6 @@
 
 /* Controllers */
 
-angular.module('portfolio',[]).controller('PortfolioCtrl', function($scope){
-
-	$scope.model ={
-		stockName:'',
-		amount:'',
-		positions:[{name:'PTT', amount:1000, price:319,total:319000}]
-	};
-
-	$scope.addToPortfolio = function(stockName, amount, positions){
-		var stockQuote = new StockQuote();
-		var price = stockQuote.getPrice(stockName); 
-		positions.push({
-			name:stockName, 
-			amount:amount,
-			price: price,
-			total: amount*price
-		});
-	};
-
-	$scope.totalValue = function(positions) {
-		var total = 0;
-		for(var k=0; k<positions.length; k++){
-			total = total + positions[k].total;
-		}
-		return total;
-	}
-});
-
-
-
 function StockQuote(){
 	this.maxPrice = 300;
 	this.minPrice = 20;
@@ -40,4 +10,42 @@ function StockQuote(){
 	}
 }
 
+var PortfolioService = function (stockQuote) {
+	this.positions = [{name:'PTTE', amount:1000, price:168, total:168000}];
+	
+	this.addToPortfolio = function(stockName, amount, positions){
+			var price = stockQuote.getPrice(stockName); 
+						
+			positions.push({
+				name:stockName, 
+				amount:amount,
+				price: price,
+				total: amount*price
+			});
+		};
 
+	this.totalValue = function(positions) {
+			var total = 0;
+			for(var k=0; k<positions.length; k++){
+				total = total + positions[k].total;
+			}
+			return total;
+		};
+}
+
+angular.module('portfolio',[])
+	.value('stockQuote', new StockQuote())	
+	.service('portfolioService', PortfolioService)
+	.controller('PortfolioCtrl', function($scope, portfolioService){
+		$scope.model ={
+			stockName:'',
+			amount:'',
+			positions:[]
+		};
+
+		$scope.model.positions = portfolioService.positions;
+		//console.log(portfolioService.positions);
+
+		$scope.addToPortfolio = portfolioService.addToPortfolio;
+		$scope.totalValue = portfolioService.totalValue;
+	});
