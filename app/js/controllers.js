ad2c1636bca4c1bb73e5d4543ff71215ecbd1d38
supatwo <supatw@gmail.com>
2013-10-10 00:07:28 +0700

    build in directive

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,6 +2,20 @@
 
 /* Controllers */
 
+angular.module('teambookManager',[])
+
+.controller('teambookCtrl', function($scope)
+{
+	$scope.teambook ={
+		newMemberName:'',
+		members :[]
+	};
+
+	$scope.teambook.addName = function(name){
+		$scope.teambook.members.push({name: name});
+	};
+
+})
 
 
 
